288faa1cf64f3312fd98bb36e7a10b11e674f3d4
unknown <josearthur.12311@gmail.com>
2024-02-29 21:46:44 -0300

    Corrigindo a geração do .exe

--- src/bin/Financial.dpr
+++ src/bin/Financial.dpr
@@ -6,18 +6,9 @@ uses
 
 {$R *.res}
 
-
-
-var
-  ProjectDir: string;
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TfrmMainPrincipal, frmMainPrincipal);
   Application.Run;
-
-
-  // Obter o diretório do arquivo de projeto
-
-  // Exibir o diretório
 end.
